abfd61ec48dc3a506e1f5d89971b8b3a1c698517
Kkiomen <kuba.owsianka@interia.pl>
2021-05-13 02:53:10 +0200

    WORK

--- src/me/kkiomen/pl/MYSQL.java
+++ src/me/kkiomen/pl/MYSQL.java
@@ -1,13 +1,13 @@
 package me.kkiomen.pl;
 
 import java.sql.*;
-import me.kkiomen.pl.FilesManager;
 import java.util.UUID;
 
 public class MYSQL {
-    final String username= String.valueOf(FilesManager.config.getString("mysql.username")); // Enter in your db username
-    final String password= String.valueOf(FilesManager.config.getString("mysql.password")); // Enter your password for the db
-    final String url = "jdbc:mysql://" + String.valueOf(FilesManager.config.getString("mysql.host")) + ":" + String.valueOf(FilesManager.config.getString("mysql.port")) + "/" + String.valueOf(FilesManager.config.getString("mysql.database")); // Enter URL with db name
+
+    final String username="root"; // Enter in your db username
+    final String password=""; // Enter your password for the db
+    final String url = "jdbc:mysql://localhost:3306/websitemc"; // Enter URL with db name
 
     //Connection vars
     public static Connection connection; //This is the variable we will use to connect to database
